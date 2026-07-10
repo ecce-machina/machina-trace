8eb13b9478e77bafc026c2f2ac09d1a73525c00c
Matt Dube <mattdube1837@gmail.com>
2026-07-10 13:49:20 -0400

    add more granularity to diskstats

--- internal/collectors/diskstats.go
+++ internal/collectors/diskstats.go
@@ -46,21 +46,36 @@ func (c *DiskstatsCollector) Collect() ([]snapshot.Source, error) {
 
 		device := fields[2]
 
-		readIOs, _ := strconv.ParseInt(fields[3], 10, 64)
-		readSectors, _ := strconv.ParseInt(fields[5], 10, 64)
+		readsCompleted, _ := strconv.ParseInt(fields[3], 10, 64)
+		readsMerged, _ := strconv.ParseInt(fields[4], 10, 64)
+		sectorsRead, _ := strconv.ParseInt(fields[5], 10, 64)
+		readTimeMS, _ := strconv.ParseInt(fields[6], 10, 64)
 
-		writeIOs, _ := strconv.ParseInt(fields[7], 10, 64)
-		writeSectors, _ := strconv.ParseInt(fields[9], 10, 64)
+		writesCompleted, _ := strconv.ParseInt(fields[7], 10, 64)
+		writesMerged, _ := strconv.ParseInt(fields[8], 10, 64)
+		sectorsWritten, _ := strconv.ParseInt(fields[9], 10, 64)
+		writeTimeMS, _ := strconv.ParseInt(fields[10], 10, 64)
+
+		iosInProgress, _ := strconv.ParseInt(fields[11], 10, 64)
+		ioTimeMS, _ := strconv.ParseInt(fields[12], 10, 64)
+		weightedIOTimeMS, _ := strconv.ParseInt(fields[13], 10, 64)
 
 		source := snapshot.Source{
 			Collector:   "proc_diskstats",
 			Object:      device,
 			TimestampNS: time.Now().UnixNano(),
 			Counters: map[string]int64{
-				"reads_completed":  readIOs,
-				"sectors_read":     readSectors,
-				"writes_completed": writeIOs,
-				"sectors_written":  writeSectors,
+				"reads_completed":     readsCompleted,
+				"reads_merged":        readsMerged,
+				"sectors_read":        sectorsRead,
+				"read_time_ms":        readTimeMS,
+				"writes_completed":    writesCompleted,
+				"writes_merged":       writesMerged,
+				"sectors_written":     sectorsWritten,
+				"write_time_ms":       writeTimeMS,
+				"ios_in_progress":     iosInProgress,
+				"io_time_ms":          ioTimeMS,
+				"weighted_io_time_ms": weightedIOTimeMS,
 			},
 		}
 
